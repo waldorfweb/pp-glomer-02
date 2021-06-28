1d5d2230ae92c20f3dc91de0ad0ef060c88951d8
waldorfweb <alex@waldorfshop.eu>
2021-06-28 12:33:34 +0200

    manual css

--- resources/js/dist/wd_custom.js
+++ resources/js/dist/wd_custom.js
@@ -22,6 +22,76 @@ $(document).ready(function() {
            $('.page-singleitem .owl-thumbs').hide();
         } else {}
     });
+
+
+//add weiter einkaufen button
+function addweitereinkaufen() {
+    setTimeout(function() {
+        if ($(".weitereinkaufen").length) {
+            //console.log('exist');
+        } else {
+            $('#add-item-to-basket-overlay .modal-footer').prepend('<button type="button" data-dismiss="modal" aria-label="Close"  class="btn btn-outline-primary btn-medium mr-2 weitereinkaufen"> <i aria-hidden="true" class="fa fa-shopping-bag"></i> weiter einkaufen </button>');
+        }
+    }, 1000);
+}
+$(".add-to-basket-container .btn").on("click", addweitereinkaufen);
+$(".add-to-basket-lg-container").on("click", addweitereinkaufen);
+
+
+//Warenkorb
+// OrderTotals find and replace - patch PMs Mehrsprachigkeit
+// $("dt").text(function(index, text) {
+//     //alert('test');
+//     var text = $(this).text();
+//     $('dt:contains("Warenwert ((Brutto))")').text('Warenwert (Brutto)');
+//     $('dt:contains("Warenwert ((Netto))")').text('Warenwert (Netto)');
+//     $('dt:contains("Versandkosten ((Brutto))")').text('Versandkosten (Brutto)');
+//     $('dt:contains("Versandkosten ((Netto))")').text('Versandkosten (Netto)');
+//     $('dt:contains("Gesamtsumme ((Brutto))")').text('Gesamtsumme (Brutto)');
+//     $('dt:contains("Gesamtsumme ((Netto))")').text('Gesamtsumme (Netto)');
+// });
+
+//Warenkorb Slidin - weiter einkaufen
+
+function addweitereinkaufen_cart() {
+
+    // OrderTotals find and replace - patch PMs Mehrsprachigkeit
+    // $("dt").text(function(index, text) {
+    //     //alert('test');
+    //     var text = $(this).text();
+    //     $('dt:contains("Warenwert ((Brutto))")').text('Warenwert (Brutto)');
+    //     $('dt:contains("Warenwert ((Netto))")').text('Warenwert (Netto)');
+    //     $('dt:contains("Versandkosten ((Brutto))")').text('Versandkosten (Brutto)');
+    //     $('dt:contains("Versandkosten ((Netto))")').text('Versandkosten (Netto)');
+    //     $('dt:contains("Gesamtsumme ((Brutto))")').text('Gesamtsumme (Brutto)');
+    //     $('dt:contains("Gesamtsumme ((Netto))")').text('Gesamtsumme (Netto)');
+    // });
+
+    setTimeout(function() {
+        if ($(".weitereinkaufen_cart").length) {
+            //console.log('exist');
+            // Close Warenkorbpreview
+            function addweitereinkaufencc() {
+                //console.log('close first');
+                $(".open-right").show("slide", { direction: "right" }, 1000);
+            }
+            $(".weitereinkaufen_cart").on("click", addweitereinkaufencc);
+        } else {
+            $('.basket-preview-footer').prepend('<button type="button" data-dismiss="modal" aria-label="Close"  class="btn btn-outline-primary btn-medium mr-2 weitereinkaufen weitereinkaufen_cart"> <i aria-hidden="true" class="fa fa-shopping-bag"></i> weiter einkaufen </button>');
+            // Close Warenkorbpreview
+            function addweitereinkaufencc() {
+                //console.log('close last');
+                $('body').removeClass('basket-open');
+                $(".open-right").show("slide", { direction: "right" }, 1000);
+            }
+            $(".weitereinkaufen_cart").on("click", addweitereinkaufencc);
+        }
+    }, 1000);
+}
+$(".toggle-basket-preview").on("click", addweitereinkaufen_cart);
+
+
+
 	
 });
 // Document Ready End
